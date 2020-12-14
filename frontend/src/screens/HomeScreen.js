3c0309b73dd686fc8acb73708da01a72eebdcffd
avamoradi <ava.moradi86@gmail.com>
2020-12-14 17:50:09 +0100

    product, rating, homescreen , header, footer

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../component/Product'
+import product from '../products'
+
+const HomeScreen = () => {
+    return (
+        <>
+        <h1>
+            <Row>
+                {product.map(product => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </h1>
+            
+        </>
+    )
+}
+
+export default HomeScreen
