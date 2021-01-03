6fce316deb583c7b5ed4c63acc2970051e1fbee4
avamoradi <ava.moradi86@gmail.com>
2021-01-03 15:50:17 +0100

    user authentication

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import  express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import {notFound, errorHandler}from './middleware/errorMiddleware.js'
 
@@ -11,10 +12,15 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('ava API is running...')
 })
+
+app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
+
 
 app.use(notFound)
 
