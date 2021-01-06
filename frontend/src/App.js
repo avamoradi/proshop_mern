abd2d8b32dd4226d325f6ff2aca33aa20d874aac
avamoradi <ava.moradi86@gmail.com>
2021-01-06 13:24:23 +0100

    frontend checkout, payment, placeorder screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,10 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+
+
 
 
 const App = () => {
@@ -18,7 +22,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          < Route path='/placeorder' component={PlaceOrderScreen}/>
           < Route path='/shipping' component={ShippingScreen}/>
+          < Route path='/payment' component={PaymentScreen }/>
           < Route path='/login' component={LoginScreen}/>
           < Route path='/register' component={RegisterScreen}/>
           < Route path='/profile' component={ProfileScreen}/>
