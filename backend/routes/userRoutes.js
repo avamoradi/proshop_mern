018fe838ebbd6fc5c3cc231ade911290672e7dee
avamoradi <ava.moradi86@gmail.com>
2021-01-13 11:28:57 +0100

    backend Admin be able to get user by id and update it

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,25 @@
 import express from 'express'
 const router = express.Router()
-import {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser} from '../controllers/userControl.js'
+import {
+       registerUser, getUsers,
+       authUser,
+       getUserProfile, updateUserProfile,
+       deleteUser, getUserById, updateUser} from '../controllers/userControl.js'
+
 import {protect, admin} from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.route('/').post(registerUser)
+      .get(protect, admin, getUsers)
+
 router.post('/login', authUser)
+
 router.route('/profile')
       .get(protect, getUserProfile)
       .put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+
+router.route('/:id')
+      .delete(protect, admin, deleteUser)
+      .get(protect, admin, getUserById)
+      .put(protect, admin, updateUser)
 
 export default router
